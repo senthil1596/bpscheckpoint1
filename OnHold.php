c97df3dc2cb508f8ab1b2014b08a803ee6b23fde
senthil1596 <35139647+senthil1596@users.noreply.github.com>
2018-11-27 19:09:45 +0530

    Update OnHold.php

--- OnHold.php
+++ OnHold.php
@@ -23,7 +23,50 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-2 bg-primary">
-				<?php include('leftNavbar.php'); ?>
+				<!-- A vertical navbar -->
+<nav class="navbar bg-primary navbar-dark">
+	<ul class="navbar-nav">
+		<li class="nav-item">
+			<a class="nav-link" href="Dashboard.php"><span class="fa fa-times-circle"></span> Dashboard</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="AccessManagement.php"><span class="fa fa-times-circle"></span> Access Management</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="SalaryRestructuring.php"><span class="fa fa-times-circle"></span> Salary Restructuring</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="Submitted.php"><span class="fa fa-times-circle"></span> Submitted</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="Cancelled.php"><span class="fa fa-times-circle"></span> Cancelled</a>
+		</li>
+		<li class="nav-item active">
+			<a class="nav-link" href="OnHold.php"><span class="fa fa-times-circle"></span> On Hold</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="ESICMinWage.php"><span class="fa fa-times-circle"></span> ESIC/Min Wage</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="LetterGenerated.php"><span class="fa fa-times-circle"></span> Letter Generated</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="LetterReleased.php"><span class="fa fa-times-circle"></span> Letter Released</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="Archive.php"><span class="fa fa-times-circle"></span> Archive</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="PosttoSAP.php"><span class="fa fa-times-circle"></span> Post to SAP</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="AuditTrail.php"><span class="fa fa-times-circle"></span> Audit Trail</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="Reports.php"><span class="fa fa-times-circle"></span> Reports</a>
+		</li>
+	</ul>
+</nav>
 			</div>
 			<div class="col-md-10"><br>
 				<b>On Hold</b>
